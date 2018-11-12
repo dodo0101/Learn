9fa0dc2ac4f302b3fa5b468181f107f91babaa9a
dodo0101 <32573628+dodo0101@users.noreply.github.com>
2018-11-12 19:08:53 +0300

    something

--- src/p5/Solution.java
+++ src/p5/Solution.java
@@ -1,10 +1,14 @@
 package p5;
 
-
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Solution  {
-	public static void main(String[] args) {
-
-		
+	public static void main(String[] args) throws FileNotFoundException, IOException {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		FileReader freader = new FileReader(reader.readLine());
 }
 }
