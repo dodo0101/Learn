e2bbdcb6ab3c2642b56dceaeb602e2b87ef4b343
dodo0101 <32573628+dodo0101@users.noreply.github.com>
2018-11-22 01:31:44 +0300

    Back to tags task

--- src/p5/Solution.java
+++ src/p5/Solution.java
@@ -5,10 +5,33 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class Solution  {
+	
 	public static void main(String[] args) throws FileNotFoundException, IOException {
-		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-		FileReader freader = new FileReader(reader.readLine());
+		
+		BufferedReader breader = new BufferedReader(new InputStreamReader(System.in));
+		FileReader freader = new FileReader(breader.readLine());
+		//C:\Users\fglus\eclipse-workspace\Learn\src\p5\test2
+		BufferedReader breader2 = new BufferedReader(freader);
+		
+		StringBuilder sbuild = new StringBuilder();
+		
+		while(breader2.ready()) {
+			sbuild.append(breader2.readLine());
+		}
+		
+		String result = sbuild.toString();
+		
+		System.out.println(result);
+		
+		breader2.close();
+		
+	}
+	
 }
-}
+	
+
+
+
